82dcf8b1dbe8fcb138733c39cb0c678d46317f46
kenzycodex <kenzycodex@gmail.com>
2024-09-13 03:11:57 +0100

    chore: update .gitignore and Vite config
    Added Vercel configuration file. Updated .gitignore to reflect necessary changes and modified vite.config.js for deployment compatibility.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the chunk size warning limit to 1000 kB
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      // Configure manual chunk splitting
+      output: {
+        manualChunks(id) {
+          // Split vendor (node_modules) files into separate chunks
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
